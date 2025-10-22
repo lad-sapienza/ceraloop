206ee4fe48e9ec799426b887cec4185b18204a41
Julian Bogdani <jbogdani@gmail.com>
2025-10-22 20:23:02 +0200

    Fix SPA routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useNavigate } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import App from './App'
 import './styles.css'
+
+// Handle redirect from 404.html
+function RedirectHandler({ children }) {
+  const navigate = useNavigate()
+  
+  React.useEffect(() => {
+    const redirectPath = sessionStorage.getItem('redirectPath')
+    if (redirectPath) {
+      sessionStorage.removeItem('redirectPath')
+      navigate(redirectPath, { replace: true })
+    }
+  }, [navigate])
+  
+  return children
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <BrowserRouter>
-        <App />
+        <RedirectHandler>
+          <App />
+        </RedirectHandler>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
